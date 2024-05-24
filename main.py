6bbde20d4ba22a02b18e481721b1ecaf6345d9e3
DracoRex679 <53967858+DracoRex679@users.noreply.github.com>
2024-05-23 20:45:09 -0700

    Diabetes Research
    Example of Diabetes Research using Line of Best Fit. Designed using Pyplot, Scikit-Learn, and Matplotlib.

--- main.py
+++ main.py
@@ -1,54 +1,62 @@
-import matplotlib.pyplot as plt
+import pandas as pd
 import numpy as np
-from sklearn import datasets, linear_model
-from sklearn.metrics import mean_squared_error, r2_score
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn import model_selection
+from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_diabetes
+from sklearn.metrics import mean_squared_error, accuracy_score, explained_variance_score
+
+if __name__ == "__main__":
+    def train(x, y):
+        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=.33)
+        print('x test:', x_test)
+        print('y test:', y_test)
+
+        a, b = np.polyfit(x_test, y_test, 1)
+        fitLine = a * x_test + b
+
+        plt.xlabel('Age')
+        plt.ylabel('Blood Sugar Level (GLU)')
+        plt.scatter(x_test, y_test, color='purple')
+        plt.plot(x_test, fitLine, color='black')
+        plt.suptitle('Diabetes Graph')
+        plt.show()
+
+        new_model = input('Would you like a new model? (Y/N)')
+        print(new_model)
+        if new_model == "Y":
+            train(x, y)
+        else:
+            search(a, b)
 
 
-def train():
-    #Use Train Test Split with randomization
-    diabetes_X_train, diabetes_X_test, diabetes_y_train, diabetes_y_test = train_test_split(diabetes_X, diabetes_y,
-                                                                                            test_size=.33)
+    def search(a, b):
+        user_data = input('What X Value would you like to search with?')
 
-    # Create linear regression object
-    regr = linear_model.LinearRegression()
+        answer = float(user_data)*a+b
+        print(answer, 'is your y-value based on the test data.')
+        searchagain = input('Would you like to search again? (Y/N)')
+        if searchagain == 'Y':
+            search(a, b)
+        else:
+            graphagain = input('Would you like a new graph? (Y/N)')
+            if graphagain == "Y":
+                train(x, y)
+            else:
+                pass
 
-    # Train the model using the training sets
-    regr.fit(diabetes_X_train, diabetes_y_train)
-
-    # Make predictions
-    diabetes_y_pred = regr.predict(diabetes_X_test)
-
-    # The coefficients
-    print("Coefficients: \n", regr.coef_)
-    # The mean squared error
-    print("Mean squared error: %.2f" % mean_squared_error(diabetes_y_test, diabetes_y_pred))
-    # The coefficient of determination: 1 is perfect prediction
-    print("Coefficient of determination: %.2f" % r2_score(diabetes_y_test, diabetes_y_pred))
-
-    # Plot outputs
-    plt.scatter(diabetes_X_test, diabetes_y_test, color="black")
-    plt.plot(diabetes_X_test, diabetes_y_pred, color="red", linewidth=3)
-
-    plt.show()
-    graphagain = input('Are you hapy with these results (Y/N)')
-    if graphagain == 'N':
-        train()
-    else:
-        return
-
-
-if __name__ == '__main__':
-    # Load the diabetes dataset
-    diabetes_X, diabetes_y = datasets.load_diabetes(return_X_y=True)
-
-    # Use only one feature
-    diabetes_X = diabetes_X[:, np.newaxis, 4]
-    print(diabetes_X)
-    print(diabetes_y)
-    train()
-
-
-
+    ddata = load_diabetes()
+    #x, y = load_diabetes(return_X_y=True)
+    df_diabetes = pd.DataFrame(data=ddata.data)
+    #print(df_diabetes.head())
+    #print(df_diabetes.shape)
+    #print(df_diabetes.columns)
+    x = df_diabetes[0]
+    #print(x)
+    y = df_diabetes[9]
+    #print(y)
+    train(x, y)
 
 
