d24bc906b2e82b9885693cc5e905baeae414e94b
DracoRex679 <53967858+DracoRex679@users.noreply.github.com>
2024-05-23 20:42:48 -0700

    Linear Regression
    Goes over simple Linear Regression using Scikit-Learn

--- main.py
+++ main.py
@@ -1,40 +1,54 @@
-import time
-from turtle import Screen
-
-import car_manager
-import scoreboard
-from player import Player
-from car_manager import CarManager
-from scoreboard import Scoreboard
-
-screen = Screen()
-screen.setup(600, 600)
-screen.tracer(0)
-
-little_t = Player()
-car_creator = car_manager.CarManager()
-level_system = Scoreboard(-80, 200)
-
-screen.listen()
-screen.onkeypress(little_t.move, "Up")
-
-playing = True
-while playing:
-    time.sleep(0.1)
-    screen.update()
-
-    car_creator.make_car()
-    car_creator.move_cars()
-    for car in car_creator.all_cars:
-        if car.distance(little_t) < 20:
-            level_system.restart()
-            little_t.restart()
-            car_creator.speed_restart()
-
-    if little_t.ycor() > 200:
-        level_system.level_up()
-        car_creator.speed_up()
-        little_t.finish()
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import datasets, linear_model
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.model_selection import train_test_split
 
 
-screen.exitonclick()
+def train():
+    #Use Train Test Split with randomization
+    diabetes_X_train, diabetes_X_test, diabetes_y_train, diabetes_y_test = train_test_split(diabetes_X, diabetes_y,
+                                                                                            test_size=.33)
+
+    # Create linear regression object
+    regr = linear_model.LinearRegression()
+
+    # Train the model using the training sets
+    regr.fit(diabetes_X_train, diabetes_y_train)
+
+    # Make predictions
+    diabetes_y_pred = regr.predict(diabetes_X_test)
+
+    # The coefficients
+    print("Coefficients: \n", regr.coef_)
+    # The mean squared error
+    print("Mean squared error: %.2f" % mean_squared_error(diabetes_y_test, diabetes_y_pred))
+    # The coefficient of determination: 1 is perfect prediction
+    print("Coefficient of determination: %.2f" % r2_score(diabetes_y_test, diabetes_y_pred))
+
+    # Plot outputs
+    plt.scatter(diabetes_X_test, diabetes_y_test, color="black")
+    plt.plot(diabetes_X_test, diabetes_y_pred, color="red", linewidth=3)
+
+    plt.show()
+    graphagain = input('Are you hapy with these results (Y/N)')
+    if graphagain == 'N':
+        train()
+    else:
+        return
+
+
+if __name__ == '__main__':
+    # Load the diabetes dataset
+    diabetes_X, diabetes_y = datasets.load_diabetes(return_X_y=True)
+
+    # Use only one feature
+    diabetes_X = diabetes_X[:, np.newaxis, 4]
+    print(diabetes_X)
+    print(diabetes_y)
+    train()
+
+
+
+
+
